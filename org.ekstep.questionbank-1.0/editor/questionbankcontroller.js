5d310f7cbcd86e69a89a1acab127a37108243d86
“Rajnish <rajnish_d@techjoomla.com>
2020-05-21 16:22:55 +0530

    Issue #SB-19388 fix : Self assessment Show Immediate Feedback should be disabled by default

--- org.ekstep.questionbank-1.0/editor/questionbankcontroller.js
+++ org.ekstep.questionbank-1.0/editor/questionbankcontroller.js
@@ -77,6 +77,7 @@ angular.module('createquestionapp', [])
       "shuffle_options": false,
       "total_items": 1
     };
+    $scope.questionSetConfigObj['show_feedback'] =  ($scope.contentType == 'SelfAssess')? false : true;
 
     $scope._constants = {
       previewPlugin: 'org.ekstep.questionset.preview',
@@ -262,6 +263,7 @@ angular.module('createquestionapp', [])
       });
 
       if (pluginInstance.editData) {
+        console.log('rajnish111')
         $scope.selectedQuestions = pluginInstance.editData.data;
         $scope.questionSetConfigObj = pluginInstance.editData.config;
         $scope.questionSetConfigObj['show_feedback'] =  ($scope.contentType == 'SelfAssess')? false : true;
